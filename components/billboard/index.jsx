600e5ff768fc31238bceb6d87305dba6408891d9
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-08-23 12:40:19 +0300

    fix billboard desing

--- components/billboard/index.jsx
+++ components/billboard/index.jsx
@@ -17,27 +17,27 @@ export default function Billboard() {
         src="/videos/dark.mp4"
         className="brightness-[70%]"
       ></video>
-      <div className="absolute top-[40%] left-6 md:left-14 w-1/2 lg:w-1/2 xl:w-1/3">
-        <p className="text-5xl sm:text-6xl md:text-7xl xl:text-6xl font-extrabold text-white">
+      <div className="absolute top-[50%] left-6 md:left-14 w-1/2 lg:w-1/2 xl:w-1/3">
+        <p className="text-xl sm:text-4xl  lg:text-5xl xl:text-5xl  font-extrabold text-white">
           {data?.title}
         </p>
-        <BillboardText />
+        <BillboardText description={data?.description} />
         <div className="flex space-x-1 md:space-x-4 mt-1 md:mt-5">
-          <div className="flex items-center justify-center md:px-3 md:py-1 bg-white hover:bg-opacity-50 px-2 py-0 sm:w-1/4 md:w-1/4 lg:w-[25%] xl:w-[35%] 2xl:w-1/4 rounded-md">
+          <div className="bg-white flex items-center justify-center py-1 sm:py-1 md:py-2 px-2 lg:px-3 hover:bg-opacity-50  xl:w-36  rounded-md">
             <FaPlay className="text-xs lg:text-2xl xl:text-3xl" />
-            <button className="ml-1 md:ml-4 text-xs sm:text-xs lg:text-lg xl:text-xl md:font-bold">
+            <button className="ml-1 md:ml-4 text-[8px] sm:text-sm lg:text-lg xl:text-xl md:font-bold">
               Play
             </button>
           </div>
-          <div className="flex items-center justify-center px-1 md:px-3 py-1 bg-gray-700 bg-opacity-40 hover:bg-opacity-20 text-white w-[40%] sm:w-1/4 sm:px-0 md:w-1/4 lg:w-[25%]  xl:w-[40%] 2xl:w-1/3 rounded-md">
-            <IoIosInformationCircleOutline className="md:text-xs lg:text-2xl xl:text-5xl" />
-            <div className="ml-1 md:ml-4 text-xs sm:text-xs md:text-xs lg:text-sm xl:text-xl  md:font-bold">
+          <div className="bg-gray-700 flex items-center justify-center py-1 sm:py-1 md:py-4 px-2 lg:px-2 bg-opacity-40 hover:bg-opacity-20 text-white  xl:w-52  rounded-md">
+            <IoIosInformationCircleOutline className="text-xs md:text-sm lg:text-xl xl:text-3xl" />
+            <div className="ml-1 md:ml-4 text-[8px] sm:text-xs md:text-xs lg:text-sm xl:text-xl  md:font-bold">
               More Info
             </div>
           </div>
         </div>
       </div>
-      <div className="border-2 w-10 h-10 absolute top-[60%] rounded-full right-20"></div>
+      <div className="border-2 w-11 h-11 absolute top-[60%] rounded-full right-36"></div>
     </div>
   );
 }
--- components/billboardText/index.jsx
+++ components/billboardText/index.jsx
@@ -1,17 +1,17 @@
 "use client";
 import React, { useEffect, useState } from "react";
-export default function BillboardText() {
+export default function BillboardText({ description }) {
   const [isVisible, setIsVisible] = useState(true);
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   useEffect(() => {
     const fadeOutTimer = setTimeout(() => {
       setIsVisible(false);
-    }, 2000);
+    }, 3000);
 
     const collapseTimer = setTimeout(() => {
       setIsCollapsed(true);
-    }, 3000);
+    }, 3500);
 
     return () => {
       clearTimeout(fadeOutTimer);
@@ -25,12 +25,11 @@ export default function BillboardText() {
       }`}
     >
       <p
-        className={`text-[8px] text-white md:text-lg mt-3 md:mt-3 w-[50%] md:w-[30%] lg:w-[50%] xl:w-[80%] drop-shadow-xl transition-opacity duration-2000 ${
+        className={`text-[8px] text-white sm:text-sm md:text-base lg:text-xl mt-0 md:mt-3 w-[80%] md:w-[70%] lg:w-[70%] xl:w-[100%] drop-shadow-xl transition-opacity duration-2000 ${
           isVisible ? "animate-slideDown" : "opacity-0"
         }`}
       >
-        Friends Proog and Emo journey inside the folds of a seemingly infinite
-        Machine, exploring the dark and twisted complex of wires
+        {description}
       </p>
     </div>
   );
