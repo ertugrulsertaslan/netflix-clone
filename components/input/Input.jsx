34a8734070a66e710daeddf00b44f56d8455d0fa
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-09-07 11:39:10 +0300

    add password visibility toggle to Input component

--- components/input/Input.jsx
+++ components/input/Input.jsx
@@ -1,29 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField } from "@mui/material";
+import { LiaEyeSolid } from "react-icons/lia";
+import { LiaEyeSlashSolid } from "react-icons/lia";
 export default function Input({ id, onChange, value, label, type }) {
+  const [showPassword, setShowPassword] = useState(true);
   const style = {
     "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "white",
+      },
+      "&:hover fieldset": {
+        borderColor: "white",
+      },
       "&.Mui-focused fieldset": {
         borderColor: "white",
       },
     },
-    "& label.Mui-focused": {
+    "& .MuiInputLabel-root": {
       color: "white",
-      borderColor: "white",
+    },
+    "& .MuiInputLabel-root.Mui-focused": {
+      color: "white",
+    },
+    "& .MuiInputBase-input": {
+      color: "white",
     },
   };
+
+  const Icon = showPassword ? LiaEyeSolid : LiaEyeSlashSolid;
+
   return (
     <div className="relative">
       <TextField
         id={id}
         onChange={onChange}
-        type={type}
+        type={showPassword ? "password" : "text"}
         value={value}
         sx={style}
-        className="text-white bg-neutral-700 w-full rounded-md"
+        className="text-white  w-full rounded-md"
         label={label}
-        variant="filled"
+        variant="outlined"
       />
+      {type === "password" && (
+        <div
+          onClick={() => setShowPassword(!showPassword)}
+          className="absolute text-white right-5 top-1/3"
+        >
+          <Icon />
+        </div>
+      )}
     </div>
   );
 }
