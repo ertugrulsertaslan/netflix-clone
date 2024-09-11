fff4f73502bd9df954aa86e24203800e2115da5d
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-09-11 13:14:28 +0300

    fix mobile video player

--- components/watch/index.jsx
+++ components/watch/index.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import useMovie from "@/hooks/useMovie";
 import { AiOutlineArrowLeft } from "react-icons/ai";
@@ -8,9 +8,26 @@ export default function WatchPage({ params }) {
   const router = useRouter();
   const { movieId } = params;
   const { data } = useMovie(movieId);
-  const [isPlaying, setIsPlaying] = useState(true);
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.matchMedia("(max-width: 767px)").matches);
+    };
+
+    checkIfMobile();
+    window.addEventListener("resize", checkIfMobile);
+
+    return () => {
+      window.removeEventListener("resize", checkIfMobile);
+    };
+  }, []);
+
   const handlePlay = () => {
-    setIsPlaying(true);
+    const videoElement = document.getElementById("video-player");
+    if (videoElement) {
+      videoElement.play();
+      setIsMobile(false);
+    }
   };
   return (
     <div className="h-screen w-screen bg-black">
@@ -25,15 +42,16 @@ export default function WatchPage({ params }) {
         </p>
       </nav>
       <video
+        id="video-player"
         onClick={handlePlay}
         className="w-full h-full lg:object-cover"
         src={data?.videoUrl}
         poster={data?.thumbnailUrl}
         controls
         playsInline
-        autoPlay={isPlaying}
+        autoPlay={!isMobile}
       ></video>
-      {!isPlaying && (
+      {isMobile && (
         <button
           onClick={handlePlay}
           className="md:hidden absolute inset-0 flex items-center justify-center bg-black bg-opacity-50"
