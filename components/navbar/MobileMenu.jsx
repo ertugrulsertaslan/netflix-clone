1e5df46742394fe9eb5b60d6bdd00867971b7312
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-09-12 12:46:44 +0300

    add link for mobile menu

--- components/navbar/MobileMenu.jsx
+++ components/navbar/MobileMenu.jsx
@@ -1,4 +1,5 @@
 import { VscTriangleUp } from "react-icons/vsc";
+import Link from "next/link";
 export default function MobileMenu({ visible }) {
   if (!visible) {
     return null;
@@ -9,15 +10,13 @@ export default function MobileMenu({ visible }) {
         <VscTriangleUp className="text-white" />
       </div>
       <div className="bg-black w-28 absolute top-5 left-0 py-5 flex-col border border-black bg-opacity-80 flex text-[6px]">
-        <div className="flex flex-col gap-3 text-white">
-          <div className="px-3 text-center hover:underline">Home</div>
-          <div className="px-3 text-center hover:underline">Series</div>
-          <div className="px-3 text-center hover:underline">Films</div>
-          <div className="px-3 text-center hover:underline">New & Popular</div>
-          <div className="px-3 text-center hover:underline">My List</div>
-          <div className="px-3 text-center hover:underline">
-            Browse by languages
-          </div>
+        <div className="flex flex-col gap-3 text-white text-center space-y-2">
+          <Link href="/">Home</Link>
+          <Link href="/">Series</Link>
+          <Link href="/">Films</Link>
+          <Link href="/">New & Popular</Link>
+          <Link href="/mylist">My List</Link>
+          <Link href="/"> Browse by languages</Link>
         </div>
       </div>
     </>
