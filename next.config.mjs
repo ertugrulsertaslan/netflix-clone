ff27a5a1356f020e0264873c66f1f9157067edec
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-09-16 17:56:18 +0300

    fix cloud public video issue

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "picsum.photos" }],
+    domains: [
+      "wallpapercave.com",
+      "dark.netflix.io",
+      "dnm.nflximg.net",
+      "occ-0-7329-1489.1.nflxso.net",
+      "occ-0-8407-2219.1.nflxso.net",
+      "wallpapers.com",
+      "images8.alphacoders.com",
+      "images3.alphacoders.com",
+      "c4.wallpaperflare.com",
+    ],
   },
 };
 
